e3bcc97f61aa89c3a46d859c8aa325f871f9a861
Stanisław Skotniczny <stanislaw.skotniczny@microsoft.wsei.edu.pl>
2023-03-14 18:58:39 +0100

    Adding ContactData class with constructor

--- PhoneBook/Program.cs
+++ PhoneBook/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace PhoneBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+        }
+
+
+
+    }
+
+}
